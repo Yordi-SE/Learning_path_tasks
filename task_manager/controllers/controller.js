9d6c4da3de5879d233aad3ba13e1fb8cb23c9055
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-14 16:17:57 +0300

    edit controllers

--- task_manager/controllers/controller.js
+++ task_manager/controllers/controller.js
@@ -2,14 +2,22 @@ const Tasks = require('../model/db.js')
 const get_by_id = (req,res)=>{
     Tasks.findOne({_id: req.params.taskId}).then((data)=>{
         if (data){
-            res.json(data)
+            res.json({
+                status: "success",
+                task: data,
+                 })
         }
         else{
-            res.status(404).send("task not found")
+            res.status(404).json({
+                status: "failed",
+                task: "task not found",
+                 })
         }
         
     }).catch((err)=>{
-        res.status(400).send("There was an error getting the task. Please try again later.")
+        res.status(400).json({
+            status: "failed",
+            task: "There was an error getting the task. Please try again later."})
     })
 }
 const patch_by_id = (req,res)=>{
@@ -21,40 +29,63 @@ const patch_by_id = (req,res)=>{
                 data.status = req.body.status
             }
             data.save()
-            res.send("success")
+            res.json({
+                status: "success",
+                task: data,
+                 })
         }
         else{
-            res.status(404).send("task not found")
+            res.status(404).json({
+                status: "failed",
+                task: "task not found",
+                 })
         }
     }).catch(()=>{
-        res.status(400).send("There was an error updating the task. Please try again later.")
+        res.status(400).json({
+            status: "failed",
+            task: "There was an error updating the task. Please try again later."})
     })
 }
 const delete_by_id = (req,res)=>{
     Tasks.findOneAndDelete({_id: req.params.taskId}).then((data)=>{
         if (data){
-            res.status(204).send("success")
+            res.status(204).send()
         }
         else{
-            res.status(404).send("task not found")
-        }
+            res.status(404).json({
+                status: "failed",
+                task: "task not found",
+                 })
+                }
         
     }).catch((error)=>{
-        res.status(500).send("There was an error deleting the task. Please try again later.")
+        res.status(400).json({
+            status: "failed",
+            task: "There was an error deleting the task. Please try again later."})
     })
 }
 const get_tasks = (req,res)=>{
     Tasks.find().then((data)=>{
-        res.json(data)
+        res.json({
+            status: "success",
+            task: data,
+             })
     }).catch((err)=>{
-        res.status(400).send("There was an error getting the tasks. Please try again later.")
+        res.status(500).json({
+            status: "failed",
+            task: "There was an error getting the task. Please try again later."})
     })
 }
 const post_tasks = (req,res)=>{
     Tasks.create(req.body).then((data)=>{
-        res.status(201).send("success")
+        res.status(201).json({
+            status: "success",
+            task: data,
+             })
     }).catch((err)=>{
-        res.status(400).send("There was an error creating the task. Please try again later.")
+        res.status(500).json({
+            status: "failed",
+            task: "There was an error creating the task. Please try again later."})
     })
 }
 module.exports = {
