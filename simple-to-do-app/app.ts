7993a30f397fd81a9565febc8b789e9c152c3693
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2024-03-21 22:04:13 +0300

    merge rebase change with remote

--- simple-to-do-app/app.ts
+++ simple-to-do-app/app.ts
@@ -5,9 +5,8 @@ import "express-async-errors";
 import { createApp } from './utils/server';
 import { db } from './model/db';
 const app:express.Application = createApp();
-const hostname: string = '127.0.0.1';
-
-const port: number = 3000;
+const hostname: string = process.env.HOSTNAME || '127.0.0.1';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 db();
 app.listen(port,hostname,()=>{
   console.log('Express Server is started at ' + port)
--- simple-to-do-app/dist/app.js
+++ simple-to-do-app/dist/app.js
@@ -1,11 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: './config.env' });
 require("express-async-errors");
 const server_1 = require("./utils/server");
 const db_1 = require("./model/db");
 const app = (0, server_1.createApp)();
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = process.env.HOSTNAME || '127.0.0.1';
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 (0, db_1.db)();
 app.listen(port, hostname, () => {
     console.log('Express Server is started at ' + port);
